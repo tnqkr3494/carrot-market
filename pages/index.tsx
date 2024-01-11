71904de7e2e0d62ca2b14bad5c71d59ea6326de7
InGwonKang <tnqkr3494@naver.com>
2024-01-11 15:20:54 +0900

    1.13 css_Auth1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="bg-[url('/vercel.svg')] dark:md:hover:bg-teal-400">
-      <h2 className="text-[97851px] text-[#000]">Hello</h2>
-    </div>
-  );
+  return null;
 };
 
 export default Home;
