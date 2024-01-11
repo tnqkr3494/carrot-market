832a5f6a28ced030cf803b52279786b923a0c07f
InGwonKang <tnqkr3494@naver.com>
2024-01-11 17:16:32 +0900

    1.15 css_Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,75 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="space-y-5 py-10">
+      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        <div key={i} className="flex justify-between border-b p-4">
+          <div className="flex">
+            <div className="mr-2 h-24 w-24 bg-gray-400" />
+            <div className="flex flex-col pt-2">
+              <h3>New iPhone 14</h3>
+              <span className="mb-2 text-sm text-gray-500">Black</span>
+              <span>$95</span>
+            </div>
+          </div>
+          <div className="flex items-end justify-end space-x-2">
+            <div className="flex items-center justify-center text-gray-500">
+              <svg
+                className="h-4 w-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                ></path>
+              </svg>
+              <span>1</span>
+            </div>
+            <div className="flex items-center justify-center text-gray-500">
+              <svg
+                className="h-4 w-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                ></path>
+              </svg>
+              <span>1</span>
+            </div>
+          </div>
+        </div>
+      ))}
+      <button className="fixed bottom-20 right-10 rounded-full bg-orange-500 p-3 text-white transition-colors hover:bg-orange-600">
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </button>
+    </div>
+  );
 };
 
 export default Home;
