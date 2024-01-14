7c6ca1b21dc301463e1496c7c34f304074a23fb6
InGwonKang <tnqkr3494@naver.com>
2024-01-14 14:54:09 +0900

    1.23 Prisma

--- components/button.tsx
+++ components/button.tsx
@@ -16,8 +16,8 @@ export default function Button({
     <button
       {...rest}
       className={cls(
-        "w-full bg-orange-500 hover:bg-orange-600 text-white  px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none",
-        large ? "py-3 text-base" : "py-2 text-sm "
+        "w-full rounded-md border border-transparent  bg-orange-500 px-4 font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2",
+        large ? "py-3 text-base" : "py-2 text-sm ",
       )}
     >
       {text}
