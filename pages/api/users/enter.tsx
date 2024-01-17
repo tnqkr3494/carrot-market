1ae7c7b5c79ba73807e6d3d020eb9b1f8759222f
InGwonKang <tnqkr3494@naver.com>
2024-01-17 10:45:44 +0900

    1.31 WithHandler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,10 @@
 import client from "@/libs/server/client";
+import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  if (req.method !== "POST") {
-    res.status(405).end();
-  }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
-  res.json({ ok: true });
+  return res.status(200).end();
 }
+
+export default withHandler("POST", handler);
