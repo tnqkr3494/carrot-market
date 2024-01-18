494125ae5ca1870257e852dd682f78829ee7fb04
InGwonKang <tnqkr3494@naver.com>
2024-01-18 12:19:14 +0900

    2.4 Auth_Token2

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,14 +1,29 @@
-import client from "@/libs/server/client";
-import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import client from "@/libs/server/client";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>,
 ) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) return res.status(404).end();
+
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   return res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password:
+    "9845904809485098594385093840598df;slkgjfdl;gkfsdjg;ldfksjgdsflgjdfklgjdflgjflkgjdgd",
+});
