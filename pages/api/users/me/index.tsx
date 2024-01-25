87f489eeafc188e5efa2c7dece795dd6200258ce
InGwonKang <tnqkr3494@naver.com>
2024-01-25 10:48:32 +0900

    4.3 Profile Edit

--- pages/api/users/me/index.tsx
+++ pages/api/users/me/index.tsx
@@ -8,13 +8,93 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>,
 ) {
-  const profile = await client.user.findUnique({
-    where: { id: req.session.user?.id },
-  });
-  res.json({
-    ok: true,
-    profile,
-  });
+  if (req.method === "GET") {
+    const profile = await client.user.findUnique({
+      where: { id: req.session.user?.id },
+    });
+    return res.json({
+      ok: true,
+      profile,
+    });
+  }
+  if (req.method === "POST") {
+    const {
+      session: { user },
+      body: { email, phone, name },
+    } = req;
+
+    const currentUser = await client.user.findUnique({
+      where: {
+        id: user?.id,
+      },
+    });
+
+    // 만약 이메일이 폼으로 들어오고, 현재 로그인되있는 유저가 이메일을 변경할 때
+    if (email && email != currentUser?.email) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            email,
+          },
+          select: {
+            id: true,
+          },
+        }),
+      );
+      if (alreadyExists) {
+        return res.json({ ok: false, error: "Email already taken" });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          email,
+        },
+      });
+      return res.json({ ok: true });
+    }
+    // 만약 폰번호가 폼으로 들어오고, 현재 로그인되있는 유저가 폰번호를 변경할 때
+    if (phone && phone != currentUser?.phone) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            phone,
+          },
+          select: {
+            id: true,
+          },
+        }),
+      );
+      if (alreadyExists) {
+        return res.json({ ok: false, error: "Phone number already taken" });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          phone,
+        },
+      });
+      return res.json({ ok: true });
+    }
+
+    if (name) {
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          name,
+        },
+      });
+      return res.json({ ok: true });
+    }
+    return res.json({ ok: true });
+  }
 }
 
-export default withApiSession(withHandler({ methods: ["GET"], handler }));
+export default withApiSession(
+  withHandler({ methods: ["GET", "POST"], handler }),
+);
