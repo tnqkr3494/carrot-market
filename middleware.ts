046144e47fd3c988e88bfb9d5f1a1a7341f4ae91
InGwonKang <tnqkr3494@naver.com>
2024-01-30 19:03:52 +0900

    4.5 Middlewares

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse, userAgent } from "next/server";
+import type { NextRequest, NextFetchEvent } from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  if (req.nextUrl.pathname.startsWith("/chats")) {
+    console.log("chats Only middleware");
+  }
+  if (req.nextUrl.pathname.startsWith("/")) {
+    const ua = userAgent(req);
+    if (ua?.isBot) {
+      return new Response("Plz don't be a bot. Be human.", { status: 403 });
+    }
+  }
+  if (req.nextUrl.pathname.startsWith("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.get("carrotsession")) {
+      NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+    }
+  }
+
+  return NextResponse.next();
+}
